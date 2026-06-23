c617019344cbb3b00ef62c92deb61770dae90d54
chalekhya2006-code <chalekhya2006@gmail.com>
2026-06-23 10:04:15 +0530

    initial commit
    Python web scraping project for tracking e-commerce product prices with SQLite storage and Streamlit dashboard visualization

---
+++ config.py
@@ -0,0 +1,11 @@
+PRODUCTS = {
+    "iPhone": "https://www.amazon.in/dp/B0GRBBPBGQ",
+    "Samsung": "https://www.amazon.in/gp/aw/d/B0FJFT4VDY"
+}
+EMAIL = "chalekhya2006@gmail.com"
+PASSWORD = "eezp gapa iyrc xrpv"
+TO_EMAIL = "chalekhya2006@gmail.com"
+
+TARGET_PRICE = 45000
+CHECK_INTERVAL = 60
+
